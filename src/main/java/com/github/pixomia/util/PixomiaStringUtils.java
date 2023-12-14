b151037cc397e9d4592cfa2b5549ffdc600ec945
Pixomia <20332831+Pixomia@users.noreply.github.com>
2023-12-14 11:43:14 +0100

    add more code and Tests: String utils

---
+++ src/main/java/com/github/pixomia/util/PixomiaStringUtils.java
@@ -0,0 +1,222 @@
+package com.github.pixomia.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PixomiaStringUtils {
+
+    private static final String SPACE = "_";
+
+    public static String buildFestLenghtStringLeft(final int toFillUp, final int len) {
+
+	return PixomiaStringUtils.buildFestLenghtStringLeft(toFillUp + "", len);
+    }
+
+    public static String buildFestLenghtStringLeft(final String toFillUp, final int len) {
+	if (len <= 0) {
+	    return "";
+	}
+	if (toFillUp.length() >= len) {
+	    return toFillUp;
+	}
+	String helper = toFillUp;
+	while (helper.length() < len) {
+	    helper = "0" + helper;
+	}
+	return helper;
+    }
+
+    public static String buildFestLenghtStringLeftSpace(final String toFillUp, final int len) {
+	if (len <= 0) {
+	    return "";
+	}
+	if (toFillUp.length() >= len) {
+	    return toFillUp;
+	}
+	String helper = toFillUp;
+	while (helper.length() < len) {
+	    helper = " " + helper;
+	}
+	return helper;
+    }
+
+    public static String buildFestLenghtStringRightSpace(final String toFillUp, final int len) {
+	if (len <= 0) {
+	    return "";
+	}
+	if (toFillUp.length() >= len) {
+	    return toFillUp;
+	}
+	String helper = toFillUp;
+	while (helper.length() < len) {
+	    helper = helper + " ";
+	}
+	return helper;
+    }
+
+    public static boolean checkIfIsLong(final String isLong) {
+
+	try {
+	    Long.parseLong(isLong.trim());
+	    return true;
+	} catch (final NumberFormatException e) {
+	    return false;
+	}
+
+    }
+
+    public static String cleanUpString(final String toClean) {
+
+	String helper = toClean.replace("\r\n", "");
+	helper = helper.replace("\n", "");
+	return helper.replace("\r", "");
+    }
+
+    /*
+     * combine two maps Key is an String and the value is an integer
+     */
+    public static Map<String, Integer> combineTwoMapsWithFrequencies(final Map<String, Integer> first,
+	    final Map<String, Integer> second) {
+	final Map<String, Integer> result = new HashMap<>();
+	for (final String currFirstKey : first.keySet()) {
+	    final Integer currFirstValue = first.get(currFirstKey);
+	    if (result.get(currFirstKey) == null) {
+		result.put(currFirstKey, currFirstValue);
+	    } else {
+		result.put(currFirstKey, result.get(currFirstKey) + currFirstValue);
+	    }
+	}
+	for (final String currSecoundKey : second.keySet()) {
+	    final Integer currSecoundValue = second.get(currSecoundKey);
+	    if (result.get(currSecoundKey) == null) {
+		result.put(currSecoundKey, currSecoundValue);
+	    } else {
+		result.put(currSecoundKey, result.get(currSecoundKey) + currSecoundValue);
+	    }
+	}
+	return result;
+    }
+
+    public static String convertCamelToSnake(final String srcCamel) {
+
+	final String helper = srcCamel.replaceAll("(\\p{Ll})(\\p{Lu})", "$1_$2");
+	return helper.toLowerCase();
+    }
+
+    public static String convertCamelToSnakeWithPrefix(final String srcCamel, final String prefix) {
+
+	String helper = "";
+	boolean prefixFound = false;
+	if (srcCamel.startsWith(prefix)) {
+	    helper = prefix.toLowerCase();
+	    prefixFound = true;
+	}
+	if (prefixFound) {
+	    return helper + PixomiaStringUtils.SPACE
+		    + srcCamel.substring(prefix.length()).replaceAll("(\\p{Ll})(\\p{Lu})", "$1_$2").toLowerCase();
+	}
+	return PixomiaStringUtils.convertCamelToSnake(srcCamel);
+    }
+
+    public static String convertSnakeToCamel(final String srcSnake) {
+
+	String helper = srcSnake;
+	if (helper.startsWith("_")) {
+	    helper = helper.substring(1);
+	}
+	if (helper.endsWith("_")) {
+	    helper = helper.substring(0, helper.length() - 1);
+	}
+	while (helper.contains(PixomiaStringUtils.SPACE)) {
+	    helper = helper.replaceFirst("_[a-z]",
+		    String.valueOf(Character.toUpperCase(helper.charAt(helper.indexOf(PixomiaStringUtils.SPACE) + 1))));
+	}
+	return helper;
+
+    }
+
+    public static String convertSnakeToTRUMP(final String srcSnake) {
+
+	return srcSnake.toUpperCase();
+    }
+
+    public static String convertToPosFileName(final String text) {
+	String help = text.trim();
+	help = help.replace(' ', '_');
+	if (help.endsWith(PixomiaStringUtils.SPACE)) {
+	    help = help.substring(0, help.length() - 1);
+	}
+	return help;
+    }
+
+    public static String cutOffString(final String toCut, final int max) {
+	if (max < 0) {
+	    return "";
+	}
+	if (toCut.length() <= max) {
+	    return toCut;
+	}
+	return toCut.substring(0, max);
+    }
+
+    public static HashMap<String, Integer> findLetterFrequence(final String text) {
+	final int one = 1;
+	final HashMap<String, Integer> result = new HashMap<>();
+	final Stream<String> stringStream = text.codePoints().mapToObj(c -> String.valueOf((char) c));
+	stringStream.forEachOrdered(curr -> {
+	    if (result.containsKey(curr)) {
+		result.put(curr, result.get(curr) + one);
+	    } else {
+		result.put(curr, one);
+	    }
+	});
+	return result;
+    }
+
+    public static String firstCharToLower(final String source) {
+
+	if (source == null) {
+	    return "";
+	}
+	if (source.length() <= 1) {
+	    return source.toLowerCase();
+	}
+	return source.substring(0, 1).toLowerCase() + source.substring(1);
+    }
+
+    public static String firstCharToUpper(final String source) {
+
+	if (source == null) {
+	    return "";
+	}
+	if (source.length() <= 1) {
+	    return source.toUpperCase();
+	}
+	return source.substring(0, 1).toUpperCase() + source.substring(1);
+    }
+
+    public static long parseToLong(final String isLong) {
+
+	return Long.parseLong(isLong.trim());
+    }
+
+    public static String removeAllAfterLast(final String toClean, final String tag) {
+	if (!toClean.contains(tag)) {
+	    return toClean;
+	}
+	return toClean.substring(0, toClean.lastIndexOf(tag));
+    }
+
+    public static String removeDoublettes(final String toClean, final String cleanFrom) {
+
+	String helper = toClean;
+	while (helper.contains(cleanFrom + cleanFrom + "")) {
+	    helper = helper.replace(cleanFrom + cleanFrom + "", cleanFrom);
+	}
+	return helper;
+    }
+}
--- src/test/java/com/github/pixomia/util/PixomiaStringUtilsTest.java
+++ src/test/java/com/github/pixomia/util/PixomiaStringUtilsTest.java
@@ -1,109 +1,155 @@
 package com.github.pixomia.util;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class PixomiaStringUtilsTest {
 
     @Test
     void testBuildFestLenghtStringLeftIntInt() {
-	fail("Not yet implemented");
-    }
-
-    @Test
-    void testBuildFestLenghtStringLeftStringInt() {
-	fail("Not yet implemented");
+	Assertions.assertEquals("", PixomiaStringUtils.buildFestLenghtStringLeft(1, 0));
+	Assertions.assertEquals("1", PixomiaStringUtils.buildFestLenghtStringLeft(1, 1));
+	Assertions.assertEquals("0001", PixomiaStringUtils.buildFestLenghtStringLeft(1, 4));
+	Assertions.assertEquals("00001", PixomiaStringUtils.buildFestLenghtStringLeft(1, 5));
     }
 
     @Test
     void testBuildFestLenghtStringLeftSpace() {
-	fail("Not yet implemented");
+	Assertions.assertEquals("", PixomiaStringUtils.buildFestLenghtStringLeftSpace("1", 0));
+	Assertions.assertEquals("1", PixomiaStringUtils.buildFestLenghtStringLeftSpace("1", 1));
+	Assertions.assertEquals("   1", PixomiaStringUtils.buildFestLenghtStringLeftSpace("1", 4));
+	Assertions.assertEquals("    1", PixomiaStringUtils.buildFestLenghtStringLeftSpace("1", 5));
     }
 
     @Test
     void testBuildFestLenghtStringRightSpace() {
-	fail("Not yet implemented");
+	Assertions.assertEquals("", PixomiaStringUtils.buildFestLenghtStringRightSpace("1", 0));
+	Assertions.assertEquals("1", PixomiaStringUtils.buildFestLenghtStringRightSpace("1", 1));
+	Assertions.assertEquals("1   ", PixomiaStringUtils.buildFestLenghtStringRightSpace("1", 4));
+	Assertions.assertEquals("1    ", PixomiaStringUtils.buildFestLenghtStringRightSpace("1", 5));
     }
 
     @Test
     void testCheckIfIsLong() {
-	fail("Not yet implemented");
+	Assertions.assertEquals(true, PixomiaStringUtils.checkIfIsLong("1"));
+	Assertions.assertEquals(false, PixomiaStringUtils.checkIfIsLong("XXX"));
+	Assertions.assertEquals(true, PixomiaStringUtils.checkIfIsLong("-11111"));
     }
 
     @Test
     void testCleanUpString() {
-	fail("Not yet implemented");
+	Assertions.assertEquals("aaabbbccc", PixomiaStringUtils.cleanUpString("aaa\r\nbbb\rccc\n"));
     }
 
     @Test
     void testCombineTwoMapsWithFrequencies() {
-	fail("Not yet implemented");
+	{
+	    final Map<String, Integer> first = new HashMap<>();
+	    first.put("BINGO", 12);
+	    final Map<String, Integer> second = new HashMap<>();
+	    second.put("FOOBAR", 23);
+	    Assertions.assertEquals(12, PixomiaStringUtils.combineTwoMapsWithFrequencies(first, second).get("BINGO"));
+	    Assertions.assertEquals(23, PixomiaStringUtils.combineTwoMapsWithFrequencies(first, second).get("FOOBAR"));
+	}
+	{
+	    final Map<String, Integer> first = new HashMap<>();
+	    first.put("BINGO", 12);
+	    final Map<String, Integer> second = new HashMap<>();
+	    second.put("BINGO", 23);
+	    Assertions.assertEquals(12 + 23,
+		    PixomiaStringUtils.combineTwoMapsWithFrequencies(first, second).get("BINGO"));
+	}
     }
 
     @Test
     void testConvertCamelToSnake() {
-	fail("Not yet implemented");
+	Assertions.assertEquals("bingo_bongo", PixomiaStringUtils.convertCamelToSnake("BingoBongo"));
+	Assertions.assertEquals("bingo", PixomiaStringUtils.convertCamelToSnake("Bingo"));
+	Assertions.assertEquals("boo", PixomiaStringUtils.convertCamelToSnake("BOO"));
     }
 
     @Test
-    void testConvertCamelToSnakeWithJK() {
-	fail("Not yet implemented");
+    void testConvertCamelToSnakeWithPrefix() {
+	Assertions.assertEquals("xy_bingo_bongo",
+		PixomiaStringUtils.convertCamelToSnakeWithPrefix("XYBingoBongo", "XY"));
+	Assertions.assertEquals("xyz_bingo_bongo",
+		PixomiaStringUtils.convertCamelToSnakeWithPrefix("XYZBingoBongo", "XYZ"));
+	Assertions.assertEquals("foobingo_bongo",
+		PixomiaStringUtils.convertCamelToSnakeWithPrefix("FOOBingoBongo", "XYZ"));
     }
 
     @Test
     void testConvertSnakeToCamel() {
-	fail("Not yet implemented");
+	Assertions.assertEquals("bingoBongo", PixomiaStringUtils.convertSnakeToCamel("bingo_bongo"));
+	Assertions.assertEquals("bingoBongo", PixomiaStringUtils.convertSnakeToCamel("bingo_bongo_"));
+	Assertions.assertEquals("bingoBongo", PixomiaStringUtils.convertSnakeToCamel("_bingo_bongo"));
     }
 
     @Test
     void testConvertSnakeToTRUMP() {
-	fail("Not yet implemented");
+	Assertions.assertEquals("BINGO_BONGO", PixomiaStringUtils.convertSnakeToTRUMP("bingo_bongo"));
     }
 
     @Test
     void testConvertToPosFileName() {
-	fail("Not yet implemented");
+	Assertions.assertEquals("la_la_la_foobar", PixomiaStringUtils.convertToPosFileName("la la la foobar     "));
+	Assertions.assertEquals("la_la_la__foobar", PixomiaStringUtils.convertToPosFileName("la la la  foobar     "));
     }
 
     @Test
     void testCutOffStringStringInt() {
-	fail("Not yet implemented");
-    }
-
-    @Test
-    void testCutOffStringStringString() {
-	fail("Not yet implemented");
+	Assertions.assertEquals("bi", PixomiaStringUtils.cutOffString("bingo_bongo", 2));
+	Assertions.assertEquals("b", PixomiaStringUtils.cutOffString("bingo_bongo", 1));
+	Assertions.assertEquals("", PixomiaStringUtils.cutOffString("bingo_bongo", 0));
+	Assertions.assertEquals("", PixomiaStringUtils.cutOffString("bingo_bongo", -1));
+	Assertions.assertEquals("bingo_bongo", PixomiaStringUtils.cutOffString("bingo_bongo", 200));
     }
 
     @Test
     void testFindLetterFrequence() {
-	fail("Not yet implemented");
+	final HashMap<String, Integer> letterFrequence = PixomiaStringUtils.findLetterFrequence("aaaaaabbbcccc");
+	Assertions.assertEquals(6, letterFrequence.get("a"));
+	Assertions.assertEquals(3, letterFrequence.get("b"));
+	Assertions.assertEquals(4, letterFrequence.get("c"));
     }
 
     @Test
     void testFirstCharToLower() {
-	fail("Not yet implemented");
+	Assertions.assertEquals("foo", PixomiaStringUtils.firstCharToLower("Foo"));
+	Assertions.assertEquals("12", PixomiaStringUtils.firstCharToLower("12"));
     }
 
     @Test
     void testFirstCharToUpper() {
-	fail("Not yet implemented");
+	Assertions.assertEquals("Foo", PixomiaStringUtils.firstCharToUpper("foo"));
+	Assertions.assertEquals("12", PixomiaStringUtils.firstCharToUpper("12"));
     }
 
     @Test
     void testParseToLong() {
-	fail("Not yet implemented");
+	Assertions.assertEquals(23, PixomiaStringUtils.parseToLong("23"));
+	Assertions.assertThrows(NumberFormatException.class, () -> {
+	    PixomiaStringUtils.parseToLong("XXX");
+	});
     }
 
     @Test
     void testRemoveAllAfterLast() {
-	fail("Not yet implemented");
+	Assertions.assertEquals("BINGO", PixomiaStringUtils.removeAllAfterLast("BINGOBINGO", "BINGO"));
+	Assertions.assertEquals("", PixomiaStringUtils.removeAllAfterLast("BINGOFoobar", "BINGO"));
+	Assertions.assertEquals("BINGOFoo", PixomiaStringUtils.removeAllAfterLast("BINGOFooXbar", "X"));
     }
 
     @Test
     void testRemoveDoublettes() {
-	fail("Not yet implemented");
+	Assertions.assertEquals("BINGO", PixomiaStringUtils.removeDoublettes("BINGOBINGO", "BINGO"));
+	Assertions.assertEquals("BINGO",
+		PixomiaStringUtils.removeDoublettes("BINGOBINGOBINGOBINGOBINGOBINGO", "BINGO"));
+	Assertions.assertEquals("BINGOXX",
+		PixomiaStringUtils.removeDoublettes("BINGOBINGOBINGOBINGOBINGOBINGOXX", "BINGO"));
     }
 
 }
